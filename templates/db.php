8e70e23c8087d21095ddb7ba3024d63acee8465d
Степан <step8507@gmail.com>
2021-12-30 18:56:01 +0500

    Продукты

--- templates/db.php
+++ templates/db.php
@@ -1,4 +1,6 @@
 <?php
 session_start();
+define('PRODUCTS_IMAGE_PATH', '/images/products/');
 $user = 'root';
 $pdo = new Pdo('mysql:dbname=fullstack2;host=127.0.0.1', $user);
+$document_root = $_SERVER['DOCUMENT_ROOT'];
