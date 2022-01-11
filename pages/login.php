8dcb828001919ea19e8097bdca339c9f5e8aa3a5
Степан <step8507@gmail.com>
2022-01-11 21:26:08 +0500

    Оповещение на странице логина

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,15 @@
 <?php
 $title = 'Авторизация';
 require_once '../templates/header.php';
+if (isset($_SESSION['loginError'])) {
+    echo 
+    "
+    <div class='alert alert-warning text-center' role='alert'>
+        Неверные данные
+    </div>
+    ";
+    unset($_SESSION['loginError']);
+}
 ?>
 <form method="POST" action="../actions/login.php">
     <input class="form-control mb-2" placeholder="Логин" name='login'>
